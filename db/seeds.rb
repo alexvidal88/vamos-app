880ede69c44f2a19fa209fb943c7c7348452100c
Armand Leleu Estrada <armandleleu47@gmail.com>
2021-07-15 20:48:40 +1000

    seeds.rb iteration for users, created a json hash

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+data = File.read('public/user_seeds.json')
+new_users = JSON.parse(data)
+new_users['users'].each do |new_user|
+  created_user = User.new(
+    fisrt_name: new_user['fisrt_name'],
+    family_name: new_user['family_name'],
+    username: new_user['username'],
+    email: new_user['email'],
+    user_id: user.id
+  )
+
+end
+
