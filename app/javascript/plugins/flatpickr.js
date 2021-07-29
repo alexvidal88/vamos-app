209464eefddc05f067198f8f934225a2e9ee0bd7
Alex Vidal <alexr.vidalh@gmail.com>
2021-07-29 16:12:19 +1000

    search

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -4,37 +4,38 @@ import flatpickr from 'flatpickr';
 
 const datePicker = () => {
   const startDateInput = document.getElementById('weather_search_start_time');
-  const endDateInput = document.getElementById('weather_search_end_time');
+  // const endDateInput = document.getElementById('weather_search_end_time');
 
   // Check that the query selector id matches the one you put around your form.
   if (startDateInput) {
-  const unavailableDates = JSON.parse(document.querySelector('#weather-search-times').dataset.unavailable)
-  endDateInput.disabled = true
+  // const unavailableDates = JSON.parse(document.querySelector('#weather-search-times').dataset.unavailable)
+  // endDateInput.disabled = true
 
-  flatpickr(startDateInput, {
-    minDate: "today",
-    maxDate: new Date().fp_incr(14),
-    // disable: unavailableDates,
-    enableTime: true,
-    // noCalendar: true,
-    dateFormat: "Y-m-d H:i",
-    // time_24hr: true
-  });
-
-  startDateInput.addEventListener("change", (e) => {
-    if (startDateInput != "") {
-      endDateInput.disabled = false
-    }
-    flatpickr(endDateInput, {
-      minDate: e.target.value,
-      // maxDate: minDate.fp_incr(14), // 14 days from now
+    flatpickr(startDateInput, {
+      minDate: "today",
+      maxDate: new Date().fp_incr(6),
       // disable: unavailableDates,
       enableTime: true,
       // noCalendar: true,
-      dateFormat: "Y-m-d H:i",
-      // time_24hr: true
-      });
-    })
+      dateFormat: "Y-m-d H",
+      time_24hr: true
+    });
+
+  // startDateInput.addEventListener("change", (e) => {
+  //   if (startDateInput != "") {
+  //     endDateInput.disabled = false
+  //   }
+  //   flatpickr(endDateInput, {
+  //     minDate: e.target.value,
+  //     // maxDate: minDate.fp_incr(14), // 14 days from now
+  //     // disable: unavailableDates,
+  //     enableTime: true,
+  //     // noCalendar: true,
+  //     dateFormat: "Y-m-d H:i",
+  //     // time_24hr: true
+  //     });
+  //   })
+  // };
   };
 };
 
