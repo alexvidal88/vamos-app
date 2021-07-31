a35f9b143acf36625563c4111a67ce6edc5a348c
Alex Vidal <alexr.vidalh@gmail.com>
2021-07-31 12:58:04 +1000

    welcome email bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :weather_searches
   has_one_attached :photo
-  after_create :send_welcome_email
+  # after_create :send_welcome_email
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
