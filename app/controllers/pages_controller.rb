b0c00bd6f799f02baf6e404772ea9d09754509d5
Alex Vidal <alexr.vidalh@gmail.com>
2021-08-07 11:06:59 +1000

    reviews

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,7 +6,7 @@ class PagesController < ApplicationController
 
   def community
     @review = Review.new
-    @searches = WeatherSearch.where.not(user_id: false)
+    @searches = WeatherSearch.where.not(user_id: nil)
     @output = @searches.map do |search|
       @date = search.start_time.split(" ")[0]
       @url = "https://api.worldweatheronline.com/premium/v1/#{search.weather_type}.ashx?key=bd3331a32f9e48a9bd834355212707&q=#{search.latitude},#{search.longitude}&format=json&tp=1&date=#{@date}"
